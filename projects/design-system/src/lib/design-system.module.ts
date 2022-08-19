ada95b2d065213a392a2690e8c028aa8fad472a8
AzriZzz <lenovorosak@gmail.com>
2022-08-19 22:32:31 +0800

    Commit: Create folder following design system

--- projects/design-system/src/lib/design-system.module.ts
+++ projects/design-system/src/lib/design-system.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import ButtonComponent from './components/button/button.component'
+import ButtonComponent from './components/atoms/button/button.component'
 
 
 @NgModule({
